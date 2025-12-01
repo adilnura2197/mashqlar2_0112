53a11bff27a09ffc4f4e51db8b9cf966630314f4
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2025-12-01 15:19:28 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+#1-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[1::2]
+print(toza)
+
+
+#2-misol
+s = "PYTHONCODE"
+print(s)
+
+toza = s[::-1]
+toza2 = s[1::2]
+print(toza2)
+
+
+#3-misol
+s = "abracadabra"
+print(s)
+
+toza = s[1:4]
+print(toza)
+
+
+#4-misol
+s = "helloslice"
+print(s)
+
+toza = s[2:7]
+print(toza)
+
+
+#5-misol
+s = "MissionComplete"
+print(s)
+
+toza = s[7::]
+print(toza)
+
+
+#6-misol
+s = "9876543210"
+print(s)
+
+toza = s[:-3]
+toza2 = toza[::-1]
+print(toza2)
+
+
+#7-misol
+s = "dictionary"
+print(s)
+
+toza = s[3:7]
+print(toza)
+
+
+#8-misol
+s = "PowerfulString"
+print(s)
+
+toza = s[5::]
+print(toza)
+
+
+#9-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[-9:-2]
+print(toza)
+
+
+#10-misol
+s = "JKLMNOPQRST"
+print(s)
+
+toza = s[-10:-5]
+print(toza)
